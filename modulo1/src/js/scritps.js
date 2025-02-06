40a4fa60ffbbd882561ab2519c776652ba09744b
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-02-06 20:58:15 -0300

    modulos diplomatura

---
+++ modulo1/src/js/scritps.js
@@ -0,0 +1,12 @@
+// JavaScript para manejar el menú responsive
+// const menuBtn = document.getElementById('menu-btn');
+// const mobileMenu = document.getElementById('mobile-menu');
+
+// menuBtn.addEventListener('click', () => {
+//     mobileMenu.classList.toggle('hidden');
+// });
+
+function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('menu-hidden');
+}
